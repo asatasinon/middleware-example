e0b624256430a8d59a4aae92082531203d49dfc4
raven.huang <raven.huang>
2024-07-04 09:41:19 +0800

    调整 example 的使用样例接口

--- example-server/src/main/java/com/raven/middleware/example/server/controller/RedisExampleController.java
+++ example-server/src/main/java/com/raven/middleware/example/server/controller/RedisExampleController.java
@@ -1,13 +1,12 @@
 package com.raven.middleware.example.server.controller;
 
+import java.text.ParseException;
+import java.time.LocalDate;
+
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
-import jakarta.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,9 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
-import com.raven.middleware.example.database.constant.RedisKeyConstant;
 import com.raven.middleware.example.server.service.RedisExampleService;
-import com.raven.middleware.example.util.JacksonUtils;
 
 /**
  * @author raven
@@ -36,6 +33,60 @@ public class RedisExampleController {
 
     @Autowired
     private RedisExampleService redisExampleService;
+
+    /**
+     * 全局计数器
+     * 请求方式: GET
+     * 请求参数: counterId
+     * 返回值: 计数器值
+     * 样例: /redis/counter/1001
+     */
+    @GetMapping("/counter/{counterId}")
+    public String getCounter(@PathVariable("counterId") String counterId) {
+        return redisExampleService.getCounter(counterId);
+    }
+
+    /**
+     * 点赞
+     * 请求方式: GET
+     * 请求参数: userId
+     * 返回值: 点赞数
+     * 样例: /redis/star/1001
+     */
+    @GetMapping("/star/{userId}")
+    public String getStarCount(@PathVariable("userId") String userId) {
+        return redisExampleService.getStarCount(userId);
+    }
+
+
+    /**
+     * 签到
+     * 请求方式: get
+     * 请求参数: userId, year
+     * 返回值: 签到结果
+     * 样例: /redis/sign-in?userId=1001&year=2024
+     */
+    @GetMapping ("/sign-in")
+    public String getSignIn(@RequestParam String userId, @RequestParam String year) {
+        return redisExampleService.getSignIn(userId, year);
+    }
+
+    /**
+     * 签到
+     * 请求方式: POST
+     * 请求参数: userId, date
+     * 返回值: 签到结果
+     * 样例: /redis/sign-in
+     * 请求体: {"userId":"1001","date":"2024-06-21"}
+     */
+    @PostMapping("/sign-in")
+    public String signIn(@RequestBody Map<String, String> data) throws ParseException {
+        String userId = data.get("userId");
+        LocalDate date = LocalDate.parse(data.get("date"));
+        int year = date.getYear();
+        int day = date.getDayOfYear();
+        return redisExampleService.signIn(userId, String.valueOf(year), day);
+    }
 
     /**
      * 购物车查询, 通过userId查询购物车信息
@@ -63,37 +114,6 @@ public class RedisExampleController {
     }
 
     /**
-     * ip访问限制, 暂定上限 5次/分钟
-     */
-    @GetMapping("/ip-limit")
-    public String getIpLimit(HttpServletRequest request) {
-        String visitorIp = request.getHeader("X-FORWARDED-FOR");
-        if (visitorIp == null || visitorIp.isEmpty() || "unknown".equalsIgnoreCase(visitorIp)) {
-            visitorIp = request.getHeader("Proxy-Client-IP");
-        }
-        if (visitorIp == null || visitorIp.isEmpty() || "unknown".equalsIgnoreCase(visitorIp)) {
-            visitorIp = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (visitorIp == null || visitorIp.isEmpty() || "unknown".equalsIgnoreCase(visitorIp)) {
-            visitorIp = request.getRemoteAddr();
-        }
-
-        return redisExampleService.getIpLimit(visitorIp);
-    }
-
-    /**
-     * 排行榜查询
-     * 请求方式: GET
-     * 请求参数: userId
-     * 返回值: 排名
-     * 样例: /redis/rank/1001
-     */
-    @GetMapping("/rank/{userId}")
-    public String getRank(@PathVariable("userId") String userId) {
-        return redisExampleService.getRank(userId);
-    }
-
-    /**
      * 获取用户信息
      * 请求方式: GET
      * 请求参数: userId
@@ -106,44 +126,18 @@ public class RedisExampleController {
     }
 
     /**
-     * 热点新闻查询
-     * 请求方式: GET
-     * 请求参数: count
-     * 返回值: 热点新闻
-     * 样例: /redis/hot-news?date=20240621&count=5
-     */
-    @GetMapping("/hot-news")
-    public String getHotNews(@RequestParam String date, @RequestParam int count) {
-        return redisExampleService.getHotNews(date, count);
-    }
-
-    /**
-     * 增加新闻热度
+     * 更新用户信息
      * 请求方式: POST
-     * 请求参数:  date, newsId, score
-     * 返回值: 热度增加结果
-     * 样例: /redis/hot-news
-     * 请求体: {"date":"20240621","newsId":"news1","score":1.0}
+     * 请求参数: userId, userInfo
+     * 返回值: 更新后的用户信息
+     * 样例: /redis/user-info
+     * 请求体: {"userId":"1001","userInfo":{"name":"张三","age":18}}
      */
-    @PostMapping("/hot-news")
-    public String updateHotNews(@RequestBody Map<String, Object> data) {
-        return redisExampleService.updateHotNews(
-            data.get("date").toString(),
-            data.get("newsId").toString(),
-            Double.parseDouble(data.get("score").toString()));
+    @PostMapping("/user-info")
+    public String updateUserInfo(@RequestBody Map<String, Object> data) {
+        return redisExampleService.updateUserInfo(data);
     }
 
-    /**
-     * 点赞
-     * 请求方式: GET
-     * 请求参数: userId
-     * 返回值: 点赞数
-     * 样例: /redis/star/1001
-     */
-    @GetMapping("/star/{userId}")
-    public String getStarCount(@PathVariable("userId") String userId) {
-        return redisExampleService.getStarCount(userId);
-    }
 
     /**
      * 获取用户关注信息
@@ -183,7 +177,6 @@ public class RedisExampleController {
         return redisExampleService.getCommonFollow(((List<String>) data.get("userIds")).toArray(new String[0]));
     }
 
-
     /**
      * 是否互相关注
      * 请求方式: get
@@ -207,5 +200,66 @@ public class RedisExampleController {
     public String mayKnow(@RequestParam String userId) {
         return redisExampleService.mayKnow(userId);
     }
+
+
+    /**
+     * 热点新闻查询
+     * 请求方式: GET
+     * 请求参数: count
+     * 返回值: 热点新闻
+     * 样例: /redis/hot-news?date=20240621&count=5
+     */
+    @GetMapping("/hot-news")
+    public String getHotNews(@RequestParam String date, @RequestParam int count) {
+        return redisExampleService.getHotNews(date, count);
+    }
+
+    /**
+     * 增加新闻热度
+     * 请求方式: POST
+     * 请求参数:  date, newsId, score
+     * 返回值: 热度增加结果
+     * 样例: /redis/hot-news
+     * 请求体: {"date":"20240621","newsId":"news1","score":1.0}
+     */
+    @PostMapping("/hot-news")
+    public String updateHotNews(@RequestBody Map<String, Object> data) {
+        return redisExampleService.updateHotNews(
+            data.get("date").toString(),
+            data.get("newsId").toString(),
+            Double.parseDouble(data.get("score").toString()));
+    }
+
+    /**
+     * ip访问限制, 暂定上限 5次/分钟
+     */
+    @GetMapping("/ip-limit")
+    public String getIpLimit(HttpServletRequest request) {
+        String visitorIp = request.getHeader("X-FORWARDED-FOR");
+        if (visitorIp == null || visitorIp.isEmpty() || "unknown".equalsIgnoreCase(visitorIp)) {
+            visitorIp = request.getHeader("Proxy-Client-IP");
+        }
+        if (visitorIp == null || visitorIp.isEmpty() || "unknown".equalsIgnoreCase(visitorIp)) {
+            visitorIp = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (visitorIp == null || visitorIp.isEmpty() || "unknown".equalsIgnoreCase(visitorIp)) {
+            visitorIp = request.getRemoteAddr();
+        }
+
+        return redisExampleService.getIpLimit(visitorIp);
+    }
+
+    /**
+     * 排行榜查询
+     * 请求方式: GET
+     * 请求参数: userId
+     * 返回值: 排名
+     * 样例: /redis/rank/1001
+     */
+    @GetMapping("/rank/{userId}")
+    public String getRank(@PathVariable("userId") String userId) {
+        return redisExampleService.getRank(userId);
+    }
+
 
 }
--- example-server/src/main/java/com/raven/middleware/example/server/service/RedisExampleService.java
+++ example-server/src/main/java/com/raven/middleware/example/server/service/RedisExampleService.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -28,6 +29,43 @@ public class RedisExampleService {
 
     @Resource
     private RedisTemplate<Object, Object> redisTemplate;
+
+
+    public String getCounter(String counterId) {
+        log.info("getCounter counterName: {}", counterId);
+        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_GLOBAL_COUNTER_KEY, counterId);
+        Long count = redisTemplate.opsForValue().increment(key);
+        return "计数器: " + counterId + " 值: " + count;
+    }
+
+    public String getStarCount(String userId) {
+        log.info("getStarCount userId: {}", userId);
+        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_STAR_COUNT_KEY, userId);
+        Long count = redisTemplate.opsForValue().increment(key);
+        return "点赞用户: " + userId + " 成功, 目前点赞数: " + count;
+    }
+
+    public String getSignIn(String userId, String year) {
+        log.info("getSignIn userId: {}, year: {}", userId, year);
+        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_SIGN_IN_KEY, year, userId);
+        Long count =  redisTemplate.execute((RedisCallback<Long>)
+            connection -> connection.stringCommands().bitCount(key.getBytes(), 0, 367)
+        );
+        return "用户: " + userId + " 签到次数: " + count;
+    }
+
+    public String signIn( String userId, String year, long day) {
+        log.info("signIn userId: {}, year: {}, day: {}", userId, year, day);
+        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_SIGN_IN_KEY, year, userId);
+
+        // 签到
+        Boolean b = redisTemplate.opsForValue().setBit(key, day, true);
+        Long count =  redisTemplate.execute((RedisCallback<Long>)
+            connection -> connection.stringCommands().bitCount(key.getBytes(), 0, 367)
+        );
+
+        return "用户: " + userId + " 签到成功, 目前签到次数: " + count;
+    }
 
     public String getShoppingCart(String userId) {
         log.info("getShoppingCart userId: {}", userId);
@@ -54,45 +92,6 @@ public class RedisExampleService {
         return JacksonUtils.obj2json(entries);
     }
 
-    public String getIpLimit(String ip) {
-        log.info("ip: {}", ip);
-        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_IP_LIMIT_KEY, ip);
-        long now = System.currentTimeMillis();
-        String formatDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date(now));
-        // 限制每分钟访问次数
-        Long count = redisTemplate.opsForZSet().count(key, now - 60 * 1000, now);
-        if (count != null && count >= IP_LIMIT_COUNT) {
-            return "当前时间为:[" + formatDate + "], "
-                   + "ip:[" + ip + "]已达访问次数上限, "
-                   + "上限为:[" + IP_LIMIT_COUNT + "次/分钟]";
-        }
-
-        // 删除历史访问记录
-        redisTemplate.opsForZSet().removeRangeByScore(key, 0, now - 60 * 1000 - 1);
-
-        // 添加访问记录
-        Boolean add = redisTemplate.opsForZSet().add(key, String.valueOf(now), now);
-
-        return "访问:" + add + ", 当前访问次数: " + (count == null ? 1 : count + 1) + "次/分钟";
-    }
-
-    public String getRank(String userId) {
-        log.info("userId: {}", userId);
-        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_RANK_KEY, "1");
-        log.info(JacksonUtils.obj2json(redisTemplate.opsForZSet().rangeWithScores(key, 0, -1)));
-
-        Long total = redisTemplate.opsForZSet().zCard(key);
-
-        Long rank = redisTemplate.opsForZSet().reverseRank(key, userId);
-
-        Double score = redisTemplate.opsForZSet().score(key, userId);
-
-        String result = rank == null ? "未上榜" : "排名: " + (rank + 1);
-
-        result += ", 您当前的分数为: " + score + ", 总榜人数: " + total;
-
-        return result;
-    }
 
     public String getUserInfo(String userId) {
         log.info("getUserInfo userId: {}", userId);
@@ -101,31 +100,25 @@ public class RedisExampleService {
         return JacksonUtils.obj2json(userInfoMap);
     }
 
-    public String getHotNews(String date, int count) {
-        log.info("date: {}, count: {}", date, count);
-        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_HOT_NEWS_KEY, date);
-        Set<Object> rankSet = redisTemplate.opsForZSet().reverseRange(key, 0, count - 1);
-        return JacksonUtils.obj2json(rankSet);
+    public String updateUserInfo(Map<String, Object> data) {
+        log.info("data={}", data);
+        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_USER_INFO_KEY, data.get("userId"));
+        Map<?, ?> userInfo = (Map<?, ?>) data.get("userInfo");
+
+        Map<String, String> collect = userInfo.entrySet().stream()
+            .collect(Collectors.toMap(
+                entry -> entry.getKey().toString(),
+                entry -> entry.getValue().toString()
+            ));
+
+        redisTemplate.opsForHash().putAll(key, collect);
+
+        Map<Object, Object> entries = redisTemplate.opsForHash().entries(key);
+
+        return JacksonUtils.obj2json(entries);
     }
 
-    public String updateHotNews(String date, String news, Double score) {
-        log.info("date: {}, news: {}", date, news);
-        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_HOT_NEWS_KEY, date);
-        // 获取旧分数
-        Double oldScore = redisTemplate.opsForZSet().score(key, news);
 
-        // 增加新分数
-        Double newScore = redisTemplate.opsForZSet().incrementScore(key, news, score);
-
-        return "新闻: " + news + ", 旧分数: " + oldScore + ", 新分数: " + newScore;
-    }
-
-    public String getStarCount(String userId) {
-        log.info("getStarCount userId: {}", userId);
-        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_STAR_COUNT_KEY, userId);
-        Long count = redisTemplate.opsForValue().increment(key);
-        return "点赞用户: " + userId + " 成功, 目前点赞数: " + count;
-    }
 
     public String getUserFollow(String userId) {
         log.info("getUserFollow userId: {}", userId);
@@ -203,5 +196,72 @@ public class RedisExampleService {
 
         return "用户: " + userId + " 可能认识的人: " + intersect;
     }
+
+
+
+    public String getHotNews(String date, int count) {
+        log.info("date: {}, count: {}", date, count);
+        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_HOT_NEWS_KEY, date);
+        Set<Object> rankSet = redisTemplate.opsForZSet().reverseRange(key, 0, count - 1);
+        return JacksonUtils.obj2json(rankSet);
+    }
+
+    public String updateHotNews(String date, String news, Double score) {
+        log.info("date: {}, news: {}", date, news);
+        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_HOT_NEWS_KEY, date);
+        // 获取旧分数
+        Double oldScore = redisTemplate.opsForZSet().score(key, news);
+
+        // 增加新分数
+        Double newScore = redisTemplate.opsForZSet().incrementScore(key, news, score);
+
+        return "新闻: " + news + ", 旧分数: " + oldScore + ", 新分数: " + newScore;
+    }
+
+    public String getIpLimit(String ip) {
+        log.info("ip: {}", ip);
+        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_IP_LIMIT_KEY, ip);
+        long now = System.currentTimeMillis();
+        String formatDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date(now));
+        // 限制每分钟访问次数
+        Long count = redisTemplate.opsForZSet().count(key, now - 60 * 1000, now);
+        if (count != null && count >= IP_LIMIT_COUNT) {
+            return "当前时间为:[" + formatDate + "], "
+                   + "ip:[" + ip + "]已达访问次数上限, "
+                   + "上限为:[" + IP_LIMIT_COUNT + "次/分钟]";
+        }
+
+        // 删除历史访问记录
+        redisTemplate.opsForZSet().removeRangeByScore(key, 0, now - 60 * 1000 - 1);
+
+        // 添加访问记录
+        Boolean add = redisTemplate.opsForZSet().add(key, String.valueOf(now), now);
+
+        return "访问:" + add + ", 当前访问次数: " + (count == null ? 1 : count + 1) + "次/分钟";
+    }
+
+    public String getRank(String userId) {
+        log.info("userId: {}", userId);
+        String key = RedisKeyConstant.formatKey(RedisKeyConstant.REDIS_RANK_KEY, "1");
+        log.info(JacksonUtils.obj2json(redisTemplate.opsForZSet().rangeWithScores(key, 0, -1)));
+
+        Long total = redisTemplate.opsForZSet().zCard(key);
+
+        Long rank = redisTemplate.opsForZSet().reverseRank(key, userId);
+
+        Double score = redisTemplate.opsForZSet().score(key, userId);
+
+        String result = rank == null ? "未上榜" : "排名: " + (rank + 1);
+
+        result += ", 您当前的分数为: " + score + ", 总榜人数: " + total;
+
+        return result;
+    }
+
+
+
+
+
+
 
 }
